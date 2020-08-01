f88204de32717b880229451bc4fd60ba68300977
Tim Cheung <timshingyu@gmail.com>
2020-08-01 09:59:33 +0800

    Updated stack

--- lib/security-stack.ts
+++ lib/security-stack.ts
@@ -1,13 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 
-import { IVpc } from '@aws-cdk/aws-ec2';
+import { ISecurityGroup, IVpc } from '@aws-cdk/aws-ec2';
 
 interface SecurityStackProps extends cdk.StackProps {
   vpc: IVpc;
 }
 
 export class SecurityStack extends cdk.Stack {
+  public readonly bastionSecurityGroup: ISecurityGroup;
+
   constructor(scope: cdk.Construct, id: string, props: SecurityStackProps) {
     super(scope, id, props);
 
@@ -23,5 +25,8 @@ export class SecurityStack extends cdk.Stack {
 
     // Allow ssh access to bastion host
     bastionSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH access');
+
+    // Assign the bastionSecurityGroup to class property
+    this.bastionSecurityGroup = bastionSecurityGroup;
   }
 }
